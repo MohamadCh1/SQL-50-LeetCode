f922d2465422ecc2609c6808a6d5b60943eaf931
MohamadCh1 <123898041+MohamadCh1@users.noreply.github.com>
2025-01-11 17:12:53 +0200

    [584] [Time Beats: 49.36%] - LeetPush

--- 584-Find-Customer-Referee.sql
+++ 584-Find-Customer-Referee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT name From Customer where referee_id is null or referee_id != 2
+SELECT name From Customer where referee_id is null or referee_id <> 2
