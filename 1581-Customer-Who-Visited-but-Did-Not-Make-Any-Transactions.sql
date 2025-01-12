ecfe33027947b622fe43779f4cb8f10739e97aed
MohamadCh1 <123898041+MohamadCh1@users.noreply.github.com>
2025-01-12 02:35:31 +0200

    [1581] [Time Beats: 26.34%] - LeetPush

--- 1581-Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
+++ 1581-Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
@@ -1,10 +1,4 @@
-# Write your MySQL query statement below
-Select customer_id,
-    Sum(case
-            when visit_id in (select visit_id from Transactions) then 0
-            else 1
-        end) as count_no_trans
-from Visits
-group by customer_id
-HAVING count_no_trans != 0
-order by count_no_trans
+SELECT customer_id,COUNT(*) as count_no_trans
+FROM Visits
+WHERE Visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
+GROUP BY customer_id
