b68329bb0a8c8bf465d7fb4dd2791c48d7fef275
MohamadCh1 <123898041+MohamadCh1@users.noreply.github.com>
2025-01-12 15:55:43 +0200

    [1633] [Time Beats: 92.42%] - LeetPush

--- 1633-Percentage-of-Users-Attended-a-Contest.sql
+++ 1633-Percentage-of-Users-Attended-a-Contest.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT R.contest_id, ROUND(COUNT(R.user_id)/ (SELECT Count(*) from Users) *100,2)as percentage
-from Register as R join Users as U on R.user_id=U.user_id
-group by R.contest_id
-order by percentage desc , R.contest_id asc
+SELECT contest_id, ROUND(COUNT(user_id)/ (SELECT Count(*) from Users) *100,2)as percentage
+from Register 
+group by contest_id
+order by percentage desc , contest_id asc
